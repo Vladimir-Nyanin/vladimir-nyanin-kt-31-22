544ed1d73c88ccf9b7280be9281473c068903c8f
vladimir-nyanin-kt-31-22 <vladimirnyanin91@gmail.com>
2025-12-05 14:01:26 +0300

    add logging and WeatherForecast test request

--- VladimirNyaninKT-31-22/Program.cs
+++ VladimirNyaninKT-31-22/Program.cs
@@ -1,21 +1,50 @@
+using NLog;
+using NLog.Web;
+
 var builder = WebApplication.CreateBuilder(args);
+var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.MapOpenApi();
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
+
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+
+    //builder.Services.AddDbContext<DisciplinaDbContext>(options =>
+    //   options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+    //builder.Services.AddServices();
+
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+    }
+
+    app.UseAuthentication();
+    app.MapControllers();
+    app.Run();
+
+}
+catch (Exception ex)
+{
+    logger.Error(ex, "Stopped program because of exception");
+
+
+}
+finally
+{
+    LogManager.Shutdown();
+
 }
 
-app.UseAuthorization();
 
-app.MapControllers();
 
-app.Run();
+
